0016ac543b8737100085e7570c8e4e4d7d18f697
Gargee Banerjee <61202425+gargee-banerjee@users.noreply.github.com>
2024-08-20 20:30:12 +0530

    Update App.js for test4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {LandingPage} from "./pages/index";
 function App() {
+  //Change for test4
   return (
       <BrowserRouter>
         <Routes>
